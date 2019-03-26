a470dd4f64c8ba2a7c9d4f9490555da48c743720
slim <dsslimshaddy@gmail.com>
2019-03-26 20:52:35 +0530

    add docker-compose-wait

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,9 @@ RUN mix local.rebar --force
 RUN mix archive.install --force https://github.com/phoenixframework/archives/raw/master/phx_new.ez
 
 WORKDIR /app
+
+## Add the wait script to the image
+ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.5.0/wait /wait
+RUN chmod +x /wait
+
 EXPOSE 4000
--- run.sh
+++ run.sh
@@ -8,14 +8,6 @@ set -e
 # Ensure the app's dependencies are installed
 mix deps.get
 
-# Wait for Postgres to become available.
-until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
-echo "\nPostgres is available: continuing with database setup..."
-
 # Potentially Set up the database
 mix ecto.reset
 
