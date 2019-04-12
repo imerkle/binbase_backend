d2a0fa9e3ddaf0d626485630d231a8625dc0135a
slim <dsslimshaddy@gmail.com>
2019-04-12 19:50:10 +0530

    test factory

--- config/config.exs
+++ config/config.exs
@@ -32,7 +32,20 @@ import_config "nouns.exs"
 config :binbase_backend,
   hashid_salt: "bhd7i7FPegcSqHlWxews",
   phx_token_salt: "user salt"
-  
+  config :binbase_backend, BinbaseBackend.Emails.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  server: "smtp.sendgrid.net",
+  hostname: "ricking06@gmail.com",
+  port: 465,
+  username: "apikey", # or {:system, "SMTP_USERNAME"}
+  password: "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", # or {:system, "SMTP_PASSWORD"}
+  tls: :if_available, # can be `:always` or `:never`
+  allowed_tls_versions: [:tlsv1, :"tlsv1.1", :"tlsv1.2"], # or {:system, "ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
+  ssl: true, # can be `true`
+  retries: 1,
+  no_mx_lookups: false, # can be `true`
+  auth: :if_available # can be `always`. If your smtp relay requires authentication set it to `always`.  
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
+
 import_config "#{Mix.env()}.exs"
